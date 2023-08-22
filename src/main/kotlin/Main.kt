d8c8db169079d99644dbdaa2c2f71f7f32f19746
Bob Dschida <rdschida@students.mchenry.edu>
2023-08-22 15:29:59 -0500

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    var aByte: Byte = 5
+    var aShort: Short = 10
+    var anInt: Int = 137
+    var aLong: Long = 1236633
+    var aFloat: Float = 3.14F
+    var aDouble: Double = 45.8876936
+    var aBoolean: Boolean = false
+    var aChar: Char = 'a'
+
+    println("""
+        The Byte is $aByte
+        The short is $aShort
+        The Int is $anInt
+        The Long is $aLong
+        The Float is $aFloat
+        The Double is $aDouble
+        The Boolean is $aBoolean
+        The Char is $aChar
+    """)
+}
